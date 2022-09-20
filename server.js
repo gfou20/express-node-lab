4733ef525882ec70686e1c04adeca8c68dff4776
GFou <gfouchard20@outlook.com>
2022-09-20 17:08:56 -0400

    add route

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/students', function(req, res) {
   res.send('<h1>Best Students Ever!</h1>')
 })
 
+app.get('/test', function(req, res) {
+  res.send('<p>No one loves tests!</p>')
+})
+
 app.listen(3000, function() {
   console.log('Hear ya on port 3000')
 })
