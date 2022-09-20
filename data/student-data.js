20e499b6cbaeed2d83ee19bd881c6829fbe637b6
GFou <gfouchard20@outlook.com>
2022-09-20 17:01:36 -0400

    create data module

---
+++ data/student-data.js
@@ -0,0 +1,11 @@
+const students = [
+  {name: 'Jasmine', passed: true, _id: 001},
+  {name: 'Jordan', passed: false, _id: 002},
+  {name: 'Mark', passed: true, _id: 003},
+  {name: 'Liz', passed: false, _id: 004},
+  {name: 'Andre', passed: false, _id: 005},
+]
+
+export {
+  students
+}
