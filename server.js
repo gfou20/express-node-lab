8826f6f12b7772cabb1d7d54ee821bede28f5e97
GFou <gfouchard20@outlook.com>
2022-09-20 16:55:16 -0400

    mount routes

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ import express  from "express"
 
 const app = express()
 
+app.get('/', function(req, res) {
+  res.send('<h1>Howdy!</h1>')
+})
 
+app.get('/students', function(req, res) {
+  res.send('<h1>Best Students Ever!</h1>')
+})
 
 app.listen(3000, function() {
   console.log('Hear ya on port 3000')
